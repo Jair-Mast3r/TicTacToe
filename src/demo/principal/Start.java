3dfa4cfb963d2b5032104c2b74a5141576e47770
Jair-Mast3r <hiram.ramirezsn@yahoo.com>
2024-07-02 01:23:16 -0600

    First commit

---
+++ src/demo/principal/Start.java
@@ -0,0 +1,15 @@
+package demo.principal;
+import demo.interfaces.*;
+
+public class Start {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		MainMenu mm = new MainMenu();
+		
+		mm.NewInterface();
+		
+	}
+
+}
